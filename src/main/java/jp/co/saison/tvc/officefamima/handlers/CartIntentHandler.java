84c09d6feed330987a84213f5e78d29b0df5c46b
H1010423 <H1010423@S14576.hulft.saison.co.jp>
2018-04-12 14:58:49 +0900

    追加

---
+++ src/main/java/jp/co/saison/tvc/officefamima/handlers/CartIntentHandler.java
@@ -0,0 +1,102 @@
+/*
+     Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+     Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file
+     except in compliance with the License. A copy of the License is located at
+
+         http://aws.amazon.com/apache2.0/
+
+     or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+     the specific language governing permissions and limitations under the License.
+*/
+
+package jp.co.saison.tvc.officefamima.handlers;
+
+import static com.amazon.ask.request.Predicates.*;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.Intent;
+import com.amazon.ask.model.IntentRequest;
+import com.amazon.ask.model.Request;
+import com.amazon.ask.model.Response;
+import com.amazon.ask.model.Slot;
+import com.amazon.ask.response.ResponseBuilder;
+
+public class CartIntentHandler implements RequestHandler {
+    public static final String ITEM_KEY = "ITEM";
+    public static final String CART_KEY = "CART";
+    public static final String ITEM_SLOT = "Item";
+
+    @Override
+    public boolean canHandle(HandlerInput input) {
+        return input.matches(intentName("CartIntent"));
+    }
+
+    @Override
+    public Optional<Response> handle(HandlerInput input) {
+        Request request = input.getRequestEnvelope().getRequest();
+        IntentRequest intentRequest = (IntentRequest) request;
+        Intent intent = intentRequest.getIntent();
+        Map<String, Slot> slots = intent.getSlots();
+
+        // Get the color slot from the list of slots.
+        Slot itemSlot = slots.get(ITEM_SLOT);
+
+        String speechText, repromptText;
+        boolean isAskResponse = false;
+
+        // Check for favorite color and create output to user.
+        if (itemSlot != null) {
+            // Store the user's favorite color in the Session and create response.
+            DBResource db = new DBResource();
+
+            String targetItem = (String) input.getAttributesManager().getSessionAttributes().get(ITEM_KEY);
+            String cartkey = (String) input.getAttributesManager().getSessionAttributes().get(CART_KEY);
+
+            if (cartkey == null) {
+                input.getAttributesManager().setSessionAttributes(Collections.singletonMap(CART_KEY, targetItem));
+            }
+
+            if (db.existItem(targetItem)) {
+        		speechText =
+        				String.format(targetItem + "をカートにいれました。");
+        		db.addItemCart(targetItem);
+            } else {
+        		speechText =
+        				"入れる商品はありません。";
+            }
+            repromptText =
+                    "You can ask me your favorite color by saying, what's my favorite color?";
+
+        } else {
+            // Render an error since we don't know what the users favorite color is.
+            speechText = "分かりません。";
+            repromptText =
+                    "I'm not sure what your favorite color is. You can tell me your favorite "
+                            + "color by saying, my color is red";
+            isAskResponse = true;
+        }
+
+        ResponseBuilder responseBuilder = input.getResponseBuilder();
+
+        responseBuilder.withSimpleCard("ColorSession", speechText)
+                .withSpeech(speechText)
+                .withShouldEndSession(false);
+
+        /*
+        if (isAskResponse) {
+            responseBuilder.withShouldEndSession(false)
+                    .withReprompt(repromptText);
+        }
+         */
+
+        return responseBuilder.build();
+    }
+
+}
---
+++ src/main/java/jp/co/saison/tvc/officefamima/handlers/DBResource.java
@@ -0,0 +1,87 @@
+package jp.co.saison.tvc.officefamima.handlers;
+
+import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
+
+public class DBResource {
+	AmazonDynamoDB client;
+	DynamoDB dynamoDB;
+	Random random;
+	String cartkey;
+
+	public DBResource() {
+		client = AmazonDynamoDBClientBuilder.standard()
+ 			.withRegion(Regions.AP_NORTHEAST_1)
+ 			.build();
+		dynamoDB = new DynamoDB(client);
+		random = new Random();
+		cartkey = String.format("ID%05d", random.nextInt(65535));
+	}
+
+	public boolean existItem(String name) {
+		if (name == null) {
+			return false;
+		}
+
+    	ScanRequest scanRequest = new ScanRequest()
+    		    .withTableName("OfficeFamima");
+
+    	ScanResult result = client.scan(scanRequest);
+
+    	for (Map<String, com.amazonaws.services.dynamodbv2.model.AttributeValue> item : result.getItems()) {
+			if (name.equals(item.get("Name").getS())) {
+				return true;
+			}
+    	}
+
+    	return false;
+	}
+
+	public String getPrice(String name) {
+    	ScanRequest scanRequest = new ScanRequest()
+    		    .withTableName("OfficeFamima");
+
+    	ScanResult result = client.scan(scanRequest);
+
+    	for (Map<String, com.amazonaws.services.dynamodbv2.model.AttributeValue> item : result.getItems()) {
+			if (name.equals(item.get("Name").getS())) {
+				return item.get("Price").getS();
+			}
+    	}
+
+    	return "";
+	}
+
+	public boolean addItemCart(String name) {
+		return addItemCart(name, "1");
+	}
+
+	public boolean addItemCart(String name, String quantity) {
+		try {
+	  		Table table = dynamoDB.getTable("OfficeFamimaCart");
+				Item item = new Item()
+						.withString("ID", UUID.randomUUID().toString()) // 毎回振る
+						.withString("SID", cartkey)
+						.withString("Name", name)
+						.withNumber("Quantity", Integer.parseInt(quantity));
+				table.putItem(item);
+				
+				return true;
+	      } catch (Exception e) {
+			System.err.println("Failed to add new attribute in " + name);
+			System.err.println(e.getStackTrace());
+	      }
+	      return false;
+	}
+}
+
